c25b33bb536192751a3a549e5cd4de7fdd2240a5
Saulo Souza <126815810+SauloSouza27@users.noreply.github.com>
2023-10-22 11:21:28 -0300

    Revert "Movement Script"
    This reverts commit 7c444be8a01f12568020e9aa540a0d4d05d095c3.

--- Assets/_Lost in Tales/Scripts/LockYAxis.cs
+++ Assets/_Lost in Tales/Scripts/LockYAxis.cs
@@ -1,41 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements.Experimental;
 
 public class LockYAxis : MonoBehaviour
 {
     private float initialYPosition;
 
-    private PlayerControler isMovingReference;
-
-    private Vector3 initialPosition;
-
-    private bool value;
-
     private void Start()
     {
         initialYPosition = transform.position.y;
-        isMovingReference = GetComponent<PlayerControler>();
-        
     }
 
     private void Update()
     {
         Vector3 newPosition = new Vector3(transform.position.x, initialYPosition, transform.position.z);
         transform.position = newPosition;
-
-    }
-
-    void OnTriggerEnter(Collider collision) {
-
-        if (collision.CompareTag("static"))
-        {
-            //If the GameObject's name matches the one you suggest, output this message in the console
-            Debug.Log("Do something here");
-            transform.position = initialPosition;
-        }
-
-        
     }
 }
--- Assets/_Lost in Tales/Scripts/PlayerControler.cs
+++ Assets/_Lost in Tales/Scripts/PlayerControler.cs
@@ -8,34 +8,19 @@ public class PlayerController : MonoBehaviour
     public float layerHeight = 1.0f;
 
     private Vector3 targetPosition;
-<<<<<<< Updated upstream
     private bool isMoving = false;
     private Quaternion targetRotation;
-=======
-    public bool isMoving = false;
-
-    public GameObject box;
->>>>>>> Stashed changes
 
     private int currentLayer = 0;
 
     private GameObject selectedBlock;
     private Vector3 sokobanBlockOffset;
 
-<<<<<<< Updated upstream
     private Animator animator;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-=======
-    private bool collisionChecks = false;
-
-    private void Start()
-    {
-        
-
->>>>>>> Stashed changes
     }
 
     private void Update()
@@ -69,15 +54,9 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        
-        collisionChecks = box.GetComponent<BoxScript>().collisionCheck;
-
-        Debug.Log(collisionChecks);
-
-        if (selectedBlock != null && !collisionChecks)
+        if (selectedBlock != null)
         {
             selectedBlock.transform.position = transform.position + sokobanBlockOffset;
-            
         }
     }
 
@@ -220,6 +199,7 @@ public class PlayerController : MonoBehaviour
             selectedBlock = null;
         }
 
+        Debug.Log("Current Layer: " + currentLayer);
     }
 
     private bool IsAdjacent(Vector3 position1, Vector3 position2)
